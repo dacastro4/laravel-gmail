16db4d9b0f7aa682b197ccd01b4190a355de7f75
Matthew Hutchings <matthewhutchings1@gmail.com>
2019-04-23 19:29:53 +0100

    Typo

--- src/Services/Message/Attachment.php
+++ src/Services/Message/Attachment.php
@@ -7,135 +7,143 @@ use Dacastro4\LaravelGmail\Traits\HasDecodableBody;
 use Google_Service_Gmail;
 use Illuminate\Support\Facades\Storage;
 
-class Attachment extends GmailConnection {
-    use HasDecodableBody;
+class Attachment extends GmailConnection
+{
+	use HasDecodableBody;
 
-    /**
-     * @var
-     */
-    public $body;
-    /**
-     * @var
-     */
-    public $id;
-    /**
-     * @var
-     */
-    public $filename;
-    /**
-     * @var
-     */
-    public $mimeType;
-    /**
-     * @var
-     */
-    public $size;
-    /**
-     * @var Google_Service_Gmail
-     */
-    private $service;
+	/**
+	 * @var
+	 */
+	public $body;
+	/**
+	 * @var
+	 */
+	public $id;
+	/**
+	 * @var
+	 */
+	public $filename;
+	/**
+	 * @var
+	 */
+	public $mimeType;
+	/**
+	 * @var
+	 */
+	public $size;
+	/**
+	 * @var Google_Service_Gmail
+	 */
+	private $service;
 
-    /**
-     * @var
-     */
-    private $messageId;
+	/**
+	 * @var
+	 */
+	private $messageId;
 
-    /**
-     * Attachment constructor.
-     *
-     * @param $singleMessageId
-     * @param \Google_Service_Gmail_MessagePart $part
-     */
-    public function __construct($singleMessageId, \Google_Service_Gmail_MessagePart $part) {
-        parent::__construct(config());
+	/**
+	 * Attachment constructor.
+	 *
+	 * @param $singleMessageId
+	 * @param \Google_Service_Gmail_MessagePart $part
+	 */
+	public function __construct( $singleMessageId, \Google_Service_Gmail_MessagePart $part )
+	{
+		parent::__construct(config());
 
-        $this->service = new Google_Service_Gmail($this);
+		$this->service = new Google_Service_Gmail( $this );
 
-        $body = $part->getBody();
-        $this->id = $body->getAttachmentId();
-        $this->size = $body->getSize();
-        $this->filename = $part->getFilename();
-        $this->mimeType = $part->getMimeType();
-        $this->messageId = $singleMessageId;
-    }
+		$body = $part->getBody();
+		$this->id = $body->getAttachmentId();
+		$this->size = $body->getSize();
+		$this->filename = $part->getFilename();
+		$this->mimeType = $part->getMimeType();
+		$this->messageId = $singleMessageId;
+	}
 
-    /**
-     * Retuns attachment ID
-     *
-     * @return string
-     */
-    public function getId() {
-        return $this->id;
-    }
+	/**
+	 * Retuns attachment ID
+	 *
+	 * @return string
+	 */
+	public function getId()
+	{
+		return $this->id;
+	}
 
-    /**
-     * Returns attachment file name
-     *
-     * @return string
-     */
-    public function getFileName() {
-        return $this->filename;
-    }
+	/**
+	 * Returns attachment file name
+	 *
+	 * @return string
+	 */
+	public function getFileName()
+	{
+		return $this->filename;
+	}
 
-    /**
-     * Returns mime type of the attachment
-     *
-     * @return string
-     */
-    public function getMimeType() {
-        return $this->mimeType;
-    }
+	/**
+	 * Returns mime type of the attachment
+	 *
+	 * @return string
+	 */
+	public function getMimeType()
+	{
+		return $this->getMimeType();
+	}
 
-    /**
-     * Returns approximate size of the attachment
-     *
-     * @return mixed
-     */
-    public function getSize() {
-        return $this->size;
-    }
+	/**
+	 * Returns approximate size of the attachment
+	 *
+	 * @return mixed
+	 */
+	public function getSize()
+	{
+		return $this->size;
+	}
 
-    /**
-     * @throws \Exception
-     */
-    public function getData() {
-        $attachment = $this->service->users_messages_attachments->get('me', $this->messageId, $this->id);
+	/**
+	 * @throws \Exception
+	 */
+	public function getData()
+	{
+		$attachment = $this->service->users_messages_attachments->get( 'me', $this->messageId, $this->id );
 
-        return $attachment->getData();
-    }
+		return $attachment->getData();
+	}
 
-    /**
-     * @param string $path
-     * @param string|null $filename
-     *
-     * @param string $disk
-     *
-     * @return string
-     * @throws \Exception
-     */
-    public function saveAttachmentTo($path = null, $filename = null, $disk = 'local') {
+	/**
+	 * @param string $path
+	 * @param string|null $filename
+	 *
+	 * @param string $disk
+	 *
+	 * @return string
+	 * @throws \Exception
+	 */
+	public function saveAttachmentTo( $path = null, $filename = null, $disk = 'local' )
+	{
 
-        $data = $this->getDecodedBody($this->getData());
+		$data = $this->getDecodedBody( $this->getData() );
 
-        if (!$data) {
-            throw new \Exception('Could not get the attachment.');
-        }
+		if ( ! $data ) {
+			throw new \Exception( 'Could not get the attachment.' );
+		}
 
-        $filename = $filename ?: $this->filename;
+		$filename = $filename ?: $this->filename;
 
-        if (is_null($path)) {
-            $path = '/';
-        } else {
-            if (!ends_with('/', $path)) {
-                $path = "{$path}/";
-            }
-        }
+		if ( is_null($path) ) {
+			$path = '/';
+		} else {
+			if ( ! ends_with( '/', $path ) ) {
+				$path = "{$path}/";
+			}
+		}
 
-        $filePathAndName = "{$path}{$filename}";
+		$filePathAndName = "{$path}{$filename}";
 
-        Storage::disk($disk)->put($filePathAndName, $data);
+		Storage::disk($disk)->put( $filePathAndName, $data );
 
-        return $filePathAndName;
+		return $filePathAndName;
 
-    }
+	}
 }
